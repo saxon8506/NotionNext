2da22b65bbae1805700f70f0e4e538f86b3ad993
James <38180010+saxon8506@users.noreply.github.com>
2026-01-04 16:49:00 +0800

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -18,7 +18,7 @@ const CONFIG = {
     '/article/brochure', // 英雄区按钮
   STARTER_HERO_BUTTON_2_TEXT: 'Follow US', // 英雄区按钮
   STARTER_HERO_BUTTON_2_URL: 'https://www.facebook.com/ofhardware', // 英雄区按钮
-  STARTER_HERO_BUTTON_2_ICON: '/images/starter/facebook-transparent-custom.svg', // 英雄区按钮2的图标，不需要则留空
+  STARTER_HERO_BUTTON_2_ICON: '/images/starter/facebook-transparent-custom.png', // 英雄区按钮2的图标，不需要则留空
 
   // 英雄区配图，如需隐藏，改为空值即可 ''
   STARTER_HERO_PREVIEW_IMAGE: '/images/starter/hero/hero-image.webp', // 产品预览图 ，默认读取public目录下图片
