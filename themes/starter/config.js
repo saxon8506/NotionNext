ecac5bd68245a0ce38fc9e2afb98645b8e5515d2
James <38180010+saxon8506@users.noreply.github.com>
2026-01-04 13:55:36 +0800

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -61,16 +61,16 @@ const CONFIG = {
 
   // 首页ABOUT区块
   STARTER_ABOUT_ENABLE: true, // ABOUT区块开关
-  STARTER_ABOUT_TITLE: '一套轻量实用的建站解决方案',
+  STARTER_ABOUT_TITLE: 'OFHardware',
   STARTER_ABOUT_TEXT:
-    'NotionNext的愿景是帮助非技术人员的小白，最低成本、最快速地搭建自己的网站，帮助您将自己的产品与故事高效地传达给世界。 <br /> <br /> 功能强大的Notion笔记，简单快速的Vercel托管平台，组成一个简单的网站',
-  STARTER_ABOUT_BUTTON_TEXT: '了解更多',
-  STARTER_ABOUT_BUTTON_URL: 'https://docs.tangly1024.com/about',
+    'OFHardware (Pinghu SinoMag Hardware Co.,Ltd) located in the beautiful scenery of the Hangjiahu,Suilun Industrial District. <br /> <br /> We supply high quality brand goods and let our customers feel best goods with best price and service.',
+  STARTER_ABOUT_BUTTON_TEXT: 'Read More',
+  STARTER_ABOUT_BUTTON_URL: '/about',
   STARTER_ABOUT_IMAGE_1: '/images/starter/about/about-image-01.jpg',
   STARTER_ABOUT_IMAGE_2: '/images/starter/about/about-image-02.jpg',
-  STARTER_ABOUT_TIPS_1: '7000+',
-  STARTER_ABOUT_TIPS_2: '博客站点',
-  STARTER_ABOUT_TIPS_3: '正在线上运行',
+  STARTER_ABOUT_TIPS_1: '100+',
+  STARTER_ABOUT_TIPS_2: 'Customers',
+  STARTER_ABOUT_TIPS_3: 'Are using our products',
 
   // 首页价格区块
   STARTER_PRICING_ENABLE: false, // 价格区块开关
